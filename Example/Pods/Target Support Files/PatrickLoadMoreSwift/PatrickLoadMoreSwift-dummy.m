447b149d16fe2bffc1aad1014dab3b1cd1bf46f7
Pratik Panchak <pratik.panchal@indianic.com>
2017-02-24 18:18:47 +0530

    initial Commit

---
+++ Example/Pods/Target Support Files/PatrickLoadMoreSwift/PatrickLoadMoreSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PatrickLoadMoreSwift : NSObject
+@end
+@implementation PodsDummy_PatrickLoadMoreSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-PatrickLoadMoreSwift_Example/Pods-PatrickLoadMoreSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PatrickLoadMoreSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PatrickLoadMoreSwift_Example
+@end
